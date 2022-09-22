8557b1a7afe7694da3ae6050884449fd7275c65a
Spinon64 <spinon@ucol.mx>
2022-09-21 22:13:09 -0500

    bmi

---
+++ ProyectosJavascript/bmi.js
@@ -0,0 +1,18 @@
+/*
+const massMark = 95 ;
+const heightMark = 1.88 ;
+const massJohn = 85 ;
+const heightJohn = 1.76 ;
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / ( heightJohn * heightJohn ) ;
+const markHigherBMI = BMIMark > BMIJohn ;
+console.log ( BMIMark , BMIJohn , markHigherBMI ) ;
+
+if(BMIMark > BMIJohn){
+    console.log(`Marks BMI ${BMIMark} is higher than Jhons`);
+
+}else{
+    console.log(`Jhons BMI ${BMIJohn} is lower than is lower thanhigher than Marks`);
+}
+
+*/
